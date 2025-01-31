d98cc3ccde6c86f7ebf40d19888b16d4670b447a
Poincare <iampengkeyao@gmail.com>
2025-01-31 17:51:01 +0100

    update

--- main.lua
+++ main.lua
@@ -1,8 +1,14 @@
+function love.load()
+  x =0
+end
 function love.draw()
     love.graphics.print("Hello World", 400, 300)
-    love.graphics.rectangle("fill",200,100,50,80)
+    love.graphics.rectangle("fill",200+x,100,50,80)
 end
 
-function cw()
+function love.update(dt)
   -- code
+  if love.keyboard.isDown("right") then
+  x=x+100*dt
+  end
 end
